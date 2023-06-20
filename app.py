42ef19ab470e7008bc5beefb7e0ff07d1fd3f3bd
Morgenni <77677292+Morgenni@users.noreply.github.com>
2023-06-20 14:11:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if uploaded_file is not None:
   detect_class = result.pandas().xyxy[0] 
   count = 0
   badcount = 0
-  for i in detect_class:
+  for i in result:
     print(i)
     if int(i["xmin"]) > int(w)/2:
       badcount += 1
